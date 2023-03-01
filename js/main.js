37cd7cb6d827580053e984be641be8932a6a56d9
Mohamed Abu Al-Saud <118353596+abu-alsaud@users.noreply.github.com>
2023-03-02 00:57:54 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,40 @@
+// بدايه اسلايد
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+// نهايه اسلايد
+
+
+
+var tickerItems = document.querySelectorAll('.news-ticker li');
+
+function tick() {
+  tickerItems[0].parentNode.appendChild(tickerItems[0]);
+}
+
+setInterval(tick, 1000);
